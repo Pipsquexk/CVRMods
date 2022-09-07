1bce93dd4d79f97ae6333616a1e9dc6923524b75
Pipsquexk <LHGames21@gmail.com>
2022-09-07 05:16:19 -0500

    Add files via upload

---
+++ EZCache/Main.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+
+using MelonLoader;
+using ABI_RC.Core.InteractionSystem;
+
+
+namespace EZCache
+{
+    public class Main : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            LoggerInstance.Msg(ConsoleColor.Blue, "EZCache is starting.....");
+            MelonCoroutines.Start(WaitForMainMenuView());
+            LoggerInstance.Msg(ConsoleColor.Green, "EZCache has started!");
+        }
+
+        /// <summary>
+        /// Called to wait for the menu view to initialize and setup EZCache ui options.
+        /// </summary>
+        /// <returns>Nothing</returns>
+        public IEnumerator WaitForMainMenuView()
+        {
+            while (ViewManager.Instance == null)
+                yield return null;
+            while (ViewManager.Instance.gameMenuView == null)
+                yield return null;
+            while (ViewManager.Instance.gameMenuView.Listener == null)
+                yield return null;
+
+            ViewManager.Instance.gameMenuView.Listener.ReadyForBindings += () =>
+            {
+                ViewManager.Instance.gameMenuView.View.RegisterForEvent("EZCache_ClearALLCache", new Action(Utilities.ClearAllCache));
+                ViewManager.Instance.gameMenuView.View.RegisterForEvent("EZCache_ClearAvatarCache", new Action(() => Utilities.ClearAvatarCache(true)));
+                ViewManager.Instance.gameMenuView.View.RegisterForEvent("EZCache_ClearPropCache", new Action(() => Utilities.ClearPropCache(true)));
+                ViewManager.Instance.gameMenuView.View.RegisterForEvent("EZCache_ClearWorldCache", new Action(() => Utilities.ClearWorldCache(true)));
+                ViewManager.Instance.gameMenuView.View.RegisterForEvent("EZCache_RestartGame", new Action(Utilities.RestartGame));
+            };
+
+            ViewManager.Instance.gameMenuView.Listener.FinishLoad += (_) =>
+            {
+                ViewManager.Instance.quickMenuView.View.ExecuteScript(@"﻿{
+            var l_block = document.createElement('div');
+
+
+
+            l_block.innerHTML = `
+
+            <div class=""settings-subcategory"">
+                <div class=""subcategory-name"">EZCache</div>
+                <div class=""subcategory-description""></div>
+            </div>
+
+            <div class=""row-wrapper"">
+                <div class=""content-btn button"" onclick=""engine.trigger('EZCache_ClearALLCache');"">Clear ALL Cache</div>
+            </div>
+            <div class=""row-wrapper"">
+                <div class=""content-btn button"" onclick=""engine.trigger('EZCache_ClearAvatarCache');"">Clear Avatar Cache</div>
+            </div>
+            <div class=""row-wrapper"">
+                <div class=""content-btn button"" onclick=""engine.trigger('EZCache_ClearPropCache');"">Clear Prop Cache</div>
+            </div>
+            <div class=""row-wrapper"">
+                <div class=""content-btn button"" onclick=""engine.trigger('EZCache_ClearWorldCache');"">Clear World Cache</div>
+            </div>
+            <div class=""row-wrapper"">
+                <div class=""content-btn button"" onclick=""engine.trigger('EZCache_RestartGame');"">Restart Game (Not Reliable)</div>
+            </div>
+            <div class=""row-wrapper""></div>
+            `;
+            document.getElementById('settings-implementation').appendChild(l_block);
+    }");
+            };
+        }
+    }
+}
---
+++ EZCache/Utilities.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+using MelonLoader;
+using ABI_RC.Core.InteractionSystem;
+
+namespace EZCache
+{
+    internal static class Utilities
+    {
+
+
+        public static string
+            avatarCachePath = "ChilloutVR_Data\\Avatars",
+            propCachePath = "ChilloutVR_Data\\Spawnables",
+            worldCachePath = "ChilloutVR_Data\\Worlds";
+
+        /// <summary>
+        /// Iterated through and deletes all files in all cache directories.
+        /// </summary>
+        public static void ClearAllCache()
+        {
+            ClearAvatarCache(false);
+            ClearPropCache(false);
+            ClearWorldCache(false);
+            ViewManager.Instance.TriggerAlert("EZCache", "Cleared all cache!", -1, true);
+        }
+
+        /// <summary>
+        /// Iterates through and deleted all files in the Avatars directory.
+        /// </summary>
+        public static void ClearAvatarCache(bool indieCall)
+        {
+            MelonLogger.Msg(ConsoleColor.Blue, "Clearing avatar cache...");
+
+            DirectoryInfo dirInfo = new DirectoryInfo(avatarCachePath);
+            FileInfo[] dirFiles = dirInfo.GetFiles();
+
+            for (int i = 0; i < dirFiles.Length; ++i)
+            {
+                try
+                {
+                    dirFiles[i].Delete();
+                }
+                catch
+                {
+                    MelonLogger.Msg(ConsoleColor.Yellow, $"Skipping {dirFiles[i].Name}..");
+                    continue;
+                }
+
+            }
+            if (indieCall) ViewManager.Instance.TriggerAlert("EZCache", "Cleared avatar cache!", -1, true);
+            MelonLogger.Msg(ConsoleColor.Green, $"Cleared avatar cache!");
+        }
+
+        /// <summary>
+        /// Iterates through and deletes all files in the Spawnables directory.
+        /// </summary>
+        public static void ClearPropCache(bool indieCall)
+        {
+            MelonLogger.Msg(ConsoleColor.Blue, "Clearing prop cache...");
+
+            DirectoryInfo dirInfo = new DirectoryInfo(propCachePath);
+            FileInfo[] dirFiles = dirInfo.GetFiles();
+
+            for (int i = 0; i < dirFiles.Length; ++i)
+            {
+                try
+                {
+                    dirFiles[i].Delete();
+                }
+                catch
+                {
+                    MelonLogger.Msg(ConsoleColor.Yellow, $"Skipping {dirFiles[i].Name}..");
+                    continue;
+                }
+
+            }
+            if (indieCall) ViewManager.Instance.TriggerAlert("EZCache", "Cleared prop cache!", -1, true);
+            MelonLogger.Msg(ConsoleColor.Green, $"Cleared prop cache!");
+        }
+
+        /// <summary>
+        /// Iterates through and deleted all files in the Worlds directory.
+        /// </summary>
+        public static void ClearWorldCache(bool indieCall)
+        {
+            MelonLogger.Msg(ConsoleColor.Blue, "Clearing world cache...");
+
+            DirectoryInfo dirInfo = new DirectoryInfo(worldCachePath);
+            FileInfo[] dirFiles = dirInfo.GetFiles();
+
+            for (int i = 0; i < dirFiles.Length; ++i)
+            {
+                try
+                {
+                    dirFiles[i].Delete();
+                }
+                catch
+                {
+                    MelonLogger.Msg(ConsoleColor.Yellow, $"Skipping {dirFiles[i].Name}..");
+                    continue;
+                }
+
+            }
+            if (indieCall) ViewManager.Instance.TriggerAlert("EZCache", "Cleared world cache!", -1, true);
+            MelonLogger.Msg(ConsoleColor.Green, $"Cleared world cache!");
+        }
+
+        /// <summary>
+        /// Starts game with the arguments it was originally started with and kills current process.
+        /// </summary>
+        public static void RestartGame()
+        {
+            Process.Start("ChilloutVR.exe", Environment.CommandLine);
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
